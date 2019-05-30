277fcbbca2b125ef23387050a8acb00cf8be130b
lihy <huanyingtoyou@live.com>
2019-05-30 20:46:39 +0800

    pom文件更新

--- view-view/src/main/java/com/lihy/view/view/application/ViewApplication.java
+++ view-view/src/main/java/com/lihy/view/view/application/ViewApplication.java
@@ -1,11 +1,8 @@
 package com.lihy.view.view.application;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
