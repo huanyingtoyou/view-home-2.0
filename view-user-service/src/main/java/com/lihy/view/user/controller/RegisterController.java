b5ec3a93cbea15503a394d7f028d73298e69d8a9
lihy <huanyingtoyou@live.com>
2019-05-31 17:09:04 +0800

    代码还原

--- view-user-service/src/main/java/com/lihy/view/user/controller/RegisterController.java
+++ view-user-service/src/main/java/com/lihy/view/user/controller/RegisterController.java
@@ -1,8 +1,5 @@
 package com.lihy.view.user.controller;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,15 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author lihy
  * @date 2018/04/17
  */
-@RefreshScope
 @RestController
 @RequestMapping("/register")
 public class RegisterController {
-    @Value("${profile}")
-    private String profile;
-
-    @GetMapping("/getName")
-    public String getName() {
-        return this.profile;
-    }
 }
