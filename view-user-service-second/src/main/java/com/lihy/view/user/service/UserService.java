07888c8ffca3e6a8030ee26b723f5612e8604f4a
lihy <huanyingtoyou@live.com>
2019-06-02 14:08:53 +0800

    代码解耦

--- view-user-service-second/src/main/java/com/lihy/view/user/service/UserService.java
+++ view-user-service-second/src/main/java/com/lihy/view/user/service/UserService.java
@@ -8,5 +8,8 @@ import com.lihy.view.common.util.ResponseResult;
  * @author lihy
  * @date 2018/04/17
  */
-public interface UserService extends UserApi {
+public interface UserService {
+    ResponseResult<User> getUserInfoByUserId(String userId);
+    ResponseResult<Void> doRegister(User user);
+    ResponseResult<Void> doLogin(User user);
 }
